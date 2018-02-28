a1b29c3c8bd587a9110b5789b1c2f8aa801a8944
jsfrench <frenche@gmail.com>
2018-02-28 15:14:16 +0000

    Refactor download test

--- swaggerhub-maven-plugin/src/test/java/io/github/jsfrench/swaggerhub/SwaggerHubDownloadTest.java
+++ swaggerhub-maven-plugin/src/test/java/io/github/jsfrench/swaggerhub/SwaggerHubDownloadTest.java
@@ -2,43 +2,30 @@ package io.github.jsfrench.swaggerhub;
 
 import io.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
-import org.apache.maven.plugin.testing.AbstractMojoTestCase;
 import org.codehaus.plexus.configuration.PlexusConfiguration;
-
 import java.io.File;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class SwaggerHubDownloadTest extends BetterAbstractMojoTestCase {
 
-    protected void setup() throws Exception {
-        super.setUp();
-    }
 
-    protected void tearDown() throws Exception {
-        super.tearDown();
+    public void testDownloadYaml() throws Exception {
+        File pom = getTestFile("src/test/resources/testProjects/download-yaml.xml");
+        runTest(pom);
     }
 
     public void testDownload() throws Exception {
         File pom = getTestFile("src/test/resources/testProjects/download.xml");
-        assertNotNull(pom);
-        assertTrue( pom.exists() );
-
-        SwaggerHubDownload swaggerHubDownload = (SwaggerHubDownload) lookupConfiguredMojo(pom, "download");
-        assertNotNull( swaggerHubDownload );
-
-        swaggerHubDownload.execute();
-        final PlexusConfiguration config = extractPluginConfiguration("swaggerhub-maven-plugin", pom);
-        String file = config.getChild("outputFile").getValue();
-
-        assertTrue(Files.isRegularFile(Paths.get(file)));
-        Swagger swagger = new SwaggerParser().read(file);
-        assertNotNull(swagger);
+        runTest(pom);
     }
 
-    public void testDownloadYaml() throws Exception {
-        File pom = getTestFile("src/test/resources/testProjects/download-yaml.xml");
+    public void testDownloadDev() throws Exception {
+        File pom = getTestFile("src/test/resources/testProjects/download-dev.xml");
+        runTest(pom);
+    }
+
+    private void runTest(File pom) throws Exception {
         assertNotNull(pom);
         assertTrue( pom.exists() );
 
