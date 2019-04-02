862fea06f2585d86b423ad626ec90e0afe5ae65b
michaelmelodysmartbear <42176254+michaelmelodysmartbear@users.noreply.github.com>
2019-04-02 13:28:24 +0100

    Addition of @RunWith(Junit4.class) to Upload/Download test classes (#31)
    * Addition of @RunWith(Junit4.class) to Upload/Download test classes
    
    * Attempting to follow pattern specified for Junit4 support in following: http://maven.apache.org/plugin-testing/maven-plugin-testing-harness/getting-started/index.html
    * Addition of @Test(expected = xyzException.class) to relevant tests
    
    * Renaming of BetterAbstractMojoTestCase to BetterMojoRule
    
    * Attempting to improve readabillity and give correct context
    
    * Swtiching to use org.junit.Assert over junit.framework for asserting and failures
    
    * Addition of Maven surefire to ensure all tests are using the same runner.

--- src/test/java/io/swagger/swaggerhub/plugin/SwaggerHubDownloadTest.java
+++ src/test/java/io/swagger/swaggerhub/plugin/SwaggerHubDownloadTest.java
@@ -3,20 +3,44 @@ package io.swagger.swaggerhub.plugin;
 import io.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
 import org.codehaus.plexus.configuration.PlexusConfiguration;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-public class SwaggerHubDownloadTest extends BetterAbstractMojoTestCase {
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+@RunWith(JUnit4.class)
+public class SwaggerHubDownloadTest {
 
+    @Rule
+    public BetterMojoRule rule = new BetterMojoRule()
+    {
+        @Override
+        protected void before() throws Throwable{
+            super.before();
+        }
+
+        @Override
+        protected void after(){
+            super.after();
+        }
+    };
+
+    @Test
     public void testDownloadYaml() throws Exception {
-        File pom = getTestFile("src/test/resources/testProjects/download-yaml.xml");
+        File pom = rule.getTestFile("src/test/resources/testProjects/download-yaml.xml");
         runTest(pom);
     }
 
+    @Test
     public void testDownload() throws Exception {
-        File pom = getTestFile("src/test/resources/testProjects/download.xml");
+        File pom = rule.getTestFile("src/test/resources/testProjects/download.xml");
         runTest(pom);
     }
 
@@ -24,11 +48,11 @@ public class SwaggerHubDownloadTest extends BetterAbstractMojoTestCase {
         assertNotNull(pom);
         assertTrue( pom.exists() );
 
-        SwaggerHubDownload swaggerHubDownload = (SwaggerHubDownload) lookupConfiguredMojo(pom, "download");
+        SwaggerHubDownload swaggerHubDownload = (SwaggerHubDownload) rule.lookupConfiguredMojo(pom, "download");
         assertNotNull( swaggerHubDownload );
 
         swaggerHubDownload.execute();
-        final PlexusConfiguration config = extractPluginConfiguration("swaggerhub-maven-plugin", pom);
+        final PlexusConfiguration config = rule.extractPluginConfiguration("swaggerhub-maven-plugin", pom);
         String file = config.getChild("outputFile").getValue();
 
         assertTrue(Files.isRegularFile(Paths.get(file)));
